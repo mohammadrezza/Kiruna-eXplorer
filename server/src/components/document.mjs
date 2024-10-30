bbe9d7e7f28afb695379d68d207fa627c2908e11
mirhajian <mrmirhajian1996@gmail.com>
2024-10-30 18:03:52 +0100

    Add components

---
+++ server/src/components/document.mjs
@@ -0,0 +1,40 @@
+import {v4 as uuidv4} from "uuid";
+
+class Document {
+    constructor() {
+        this.id = '';
+        this.title = '';
+        this.stakeholders = '';
+        this.scale = '';
+        this.issuanceDate = '';
+        this.type = '';
+        this.language = '';
+        this.coordinates = '';
+        this.connections = '';
+        this.createdAt = '';
+    }
+    createFromObject(obj) {
+        this.id = uuidv4();
+        this.title = obj.title;
+        this.stakeholders = obj.stakeholders;
+        this.scale = obj.scale;
+        this.issuanceDate = obj.issuanceDate;
+        this.type = obj.type;
+        this.language = obj.language;
+        this.coordinates = obj.coordinates;
+        this.connections = obj.connections;
+    }
+
+    createFromDatabaseRow(row) {
+        this.id = row.id;
+        this.title = row.title;
+        this.stakeholders = row.stakeholders;
+        this.scale = row.scale;
+        this.issuanceDate = row.issuance_date;
+        this.type = row.type;
+        this.language = row.language;
+        this.coordinates = row.coordinates;
+        this.connections = row.connections;
+        this.createdAt = row.created_at;
+    }
+}
