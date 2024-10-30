6a2a90e7b14d8bf26d76db6e3b55dd4e7111cb53
SIMONE ANEDDA s330431 <s330431@studenti.polito.it>
2024-10-30 11:55:31 +0100

    edit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom';
 import AddDocument from './components/AddDocument';
+import Homepage from './components/Homepage';
 
 function App() {
   return (
    <Routes>
+      <Route path='/' element={
+          <Homepage/>
+      } />
       <Route path='/newdoc' element={
           <AddDocument/>
       } />
