d246ef924867bec0de4c7bdbe039b4b4bc67812b
SIMONE ANEDDA s330431 <s330431@studenti.polito.it>
2024-11-03 14:51:02 +0100

    get documents and get related documents API and some adjustments for the linking documents

--- client/src/services/API.mjs
+++ client/src/services/API.mjs
@@ -36,18 +36,63 @@ async function getTypes() {
     const types = ['serv1','serv2','serv3'];
     return types;
 }
-async function getDocuments(){
-    const doc= [];
-    doc.push(new Document ("1","title", "stakeholder", "scale", "01/01/1999", "type", "language", "description"));
-    doc.push(new Document ("2","title2", "stakeholder2", "scale2", "01/01/1999", "type2", "language2", "description2"));
-    doc.push(new Document ("3","title3", "stakeholder3", "scale3", "01/01/1999", "type3", "language3", "description3"));
-    return doc;
+// async function getDocuments(){
+//     const doc= [];
+//     doc.push(new Document ("1","title", "stakeholder", "scale", "01/01/1999", "type", "language", "description"));
+//     doc.push(new Document ("2","title2", "stakeholder2", "scale2", "01/01/1999", "type2", "language2", "description2"));
+//     doc.push(new Document ("3","title3", "stakeholder3", "scale3", "01/01/1999", "type3", "language3", "description3"));
+//     return doc;
+// }
+async function getDocuments() {
+    try {
+        const response = await fetch(`${url}/documents`, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            const documents = await response.json();
+            return documents;
+        } else {
+            const errDetail = await response.json();
+            if (errDetail.error)
+                throw errDetail.error;
+            if (errDetail.message)
+                throw errDetail.message;
+
+            throw "Something went wrong while fetching documents.";
+        }
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+async function getRelatedDocuments(docID) {
+    try {
+        const response = await fetch(`${url}/documents/${docID}/related`, {
+            method: "GET",
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (response.ok) {
+            const relatedDocs = await response.json();
+            return relatedDocs; 
+        } else {
+            const errorDetail = await response.json();
+            throw errorDetail.error || errorDetail.message || "Failed to fetch related documents.";
+        }
+    } catch (error) {
+        console.error("Error fetching related documents:", error);
+        throw error;
+    }
 }
 
 async function getData(id) {
     console.log(id);
     return new Document ("1","title", "stakeholder", "scale", "01/01/1999", "type", "language", "description");
 }
-const API ={AddDocumentDescription, getTypes, getDocuments, getData}
+const API ={AddDocumentDescription, getTypes, getDocuments, getData, getRelatedDocuments}
 
 export default API;
