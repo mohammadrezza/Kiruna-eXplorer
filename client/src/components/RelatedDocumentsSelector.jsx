fbc70def454638643b832a679763bf3e344d809d
Tomiris <s330029@studenti.polito.it>
2024-11-06 01:32:40 +0100

    fix: RelatedDocumentsSelector

--- client/src/components/RelatedDocumentsSelector.jsx
+++ client/src/components/RelatedDocumentsSelector.jsx
@@ -1,82 +1,51 @@
 // RelatedDocumentsSelector.js
 import React, { useState } from 'react';
-import { Button, ListGroup, Row, Col, Form } from 'react-bootstrap';
+import { ListGroup, Row, Col, Form } from 'react-bootstrap';
 import dayjs from 'dayjs';
 
 function RelatedDocumentsSelector({
   mode,
   edit,
   allDocuments,
-  relatedDocuments,
   selectedDocuments,
   onDocumentSelect,
   onRelatedDocumentClick
 }) {
-  const [showDocumentList, setShowDocumentList] = useState(false);
 
   return (
-    <div>
-      {(mode === 'add' || edit) ? (
-        !showDocumentList ? (
-          <Button 
-            className="selectrelated" 
-            variant="dark" 
-            onClick={() => setShowDocumentList(true)}
-          >
-            Select Related Documents
-          </Button>
-        ) : (
-          <div className="document-list">
-            <h5>Select Related Documents</h5>
-            <ListGroup className='relateddocs'>
-              {allDocuments.map((doc) => (
-                <ListGroup.Item key={doc.id}>
-                  <Form.Check 
-                    type="checkbox"
-                    label={
-                      <Row>
-                        <Col>{doc.title}</Col>
-                        <Col>{doc.stakeholder}</Col>
-                        <Col>{dayjs(doc.issuanceDate).format('DD/MM/YYYY')}</Col>
-                      </Row>
-                    }
-                    checked={selectedDocuments.includes(doc.id)}
-                    onChange={() => onDocumentSelect(doc.id)}
-                  />
-                </ListGroup.Item>
-              ))}
-            </ListGroup>
-            <Button 
-              className="saveselection" 
-              variant="dark" 
-              onClick={() => setShowDocumentList(false)}
+    <div className="document-list">
+      <ListGroup className='relatedDocs'>
+        <ListGroup.Item className='relatedDocs-header'>
+          <Row>
+            <Col md={1}></Col>
+            <Col md={4}>Title</Col>
+            <Col md={4}>Stakeholders</Col>
+            <Col md={3}>Issuance Date</Col>
+          </Row>
+        </ListGroup.Item>
+        {allDocuments.map((doc, num) => (
+          <ListGroup.Item 
+            key={doc.id} 
+            className={selectedDocuments.includes(doc.id) ? 'selected' : ''}
+            onClick={() => {(mode === 'add' || edit) ? onDocumentSelect(doc.id) : onRelatedDocumentClick(doc.id)}} 
             >
-              Save Selection
-            </Button>
-          </div>
-        )
-      ) : (
-        <div className="document-list">
-          <h5>Related Documents</h5>
-          <ListGroup className='relateddocs'>
-            {relatedDocuments.map((relatedDoc) => (
-              relatedDoc && (
-                <ListGroup.Item 
-                  key={relatedDoc.id}
-                  action
-                  onClick={() => onRelatedDocumentClick(relatedDoc.id)}
-                >
-                  <Row>
-                    <Col>{relatedDoc.title}</Col>
-                    <Col>{relatedDoc.stakeholder}</Col>
-                    <Col>{dayjs(relatedDoc.issuanceDate).format('DD/MM/YYYY')}</Col>
-                  </Row>
-                </ListGroup.Item>
-              )
-            ))}
-          </ListGroup>
-        </div>
-      )}
+            <Row className="align-items-center">
+              <Col md={1} className="text-center">
+                {(mode === 'add' || edit) ? 
+                <Form.Check 
+                  type="checkbox"
+                  id={`checkbox-${doc.id}`} 
+                  checked={selectedDocuments.includes(doc.id)}
+                />
+              : num + 1}
+              </Col>
+              <Col md={4}>{doc.title}</Col>
+              <Col md={4}>{doc.stakeholders}</Col>
+              <Col md={3}>{dayjs(doc.issuanceDate).format('DD/MM/YYYY')}</Col>
+            </Row>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
     </div>
   );
 }
