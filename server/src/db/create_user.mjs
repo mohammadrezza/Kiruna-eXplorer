9670aa05f1db5ca4a721d9b219ba40ca90c29726
mirhajian <mrmirhajian1996@gmail.com>
2024-11-11 17:18:15 +0100

    Add create_user.mjs to create sample users

---
+++ server/src/db/create_user.mjs
@@ -0,0 +1,29 @@
+import sqlite from "sqlite3";
+import crypto from "crypto";
+import UserType from "../components/userType.mjs";
+
+const database = new sqlite.Database('./db.db', (err) => {
+    if (err) throw err;
+});
+
+const users = [
+    {username: "resident", password: "102030", role: UserType.RESIDENT},
+    {username: "visitor", password: "405060", role: UserType.VISITOR},
+    {username: "urban_planner", password: "708090", role: UserType.URBAN_PLANNER},
+]
+
+await database.run("DELETE FROM User", [],
+    (err) => {
+        if (err) throw err;
+    });
+
+for (const user of users) {
+    const {username, password, role} = user;
+    const salt = crypto.randomBytes(16).toString("hex");
+    const hashedPassword = crypto.scryptSync(password, salt, 16).toString("hex");
+    await database.run("INSERT INTO User (username, password, role, salt) VALUES (?, ?, ?, ?)",
+        [username, hashedPassword, role, salt],
+        (err) => {
+            if (err) throw err;
+        });
+}
