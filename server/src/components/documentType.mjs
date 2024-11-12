002fa2e066d9fc886fc6fe0c878fab339b380846
Alice Santoro s324739 <s324739@studenti.polito.it>
2024-11-12 21:41:02 +0100

    add all document types

--- server/src/components/documentType.mjs
+++ server/src/components/documentType.mjs
@@ -3,7 +3,10 @@ const DocumentType = {
     PRESCRIPTIVE_DOCUMENT: 'Prescriptive Document',
     DESIGN_DOCUMENT: 'Design Document',
     TECHNICAL_DOCUMENT: 'Technical Document',
-    INFORMATIVE_DOCUMENT: 'Informative Document'
+    INFORMATIVE_DOCUMENT: 'Informative Document', 
+    AGREEMENT: 'Agreement',
+    CONFLICT: 'Conflict',
+    CONSULTATION: 'Consultation'
 };
 
 Object.freeze(DocumentType)
