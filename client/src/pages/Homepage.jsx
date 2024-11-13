7fc622ee556c7f9e6e720085019d1fcbdcf91c2e
SIMONE ANEDDA s330431 <s330431@studenti.polito.it>
2024-11-13 12:58:00 +0100

    slideshow component

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,20 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import '../style/Homepage.css';
 import { useNavigate } from 'react-router-dom';
+import Slideshow from '../components/Slideshow';
 
 function Homepage() {
   const navigate = useNavigate();
-
-  const images = ['/kiruna.png', '/kiruna2.jpeg', '/kiruna3.jpeg', '/kiruna4.webp']; // Add paths to more images here
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 5000); 
-
-    return () => clearInterval(interval); 
-  }, [images.length]);
 
   const handleNewDocument = () => {
     navigate(`/documents/add`);
@@ -26,13 +16,7 @@ function Homepage() {
         <h2>Welcome to</h2>
         <h1>Kiruna</h1>
         <p>Sweden</p>
-        <div className="image-container">
-          <img
-            src={images[currentImageIndex]}
-            alt="Kiruna Slideshow"
-            className="slideshow-image fade-in-out" 
-          />
-        </div>
+        <Slideshow></Slideshow>
       </div>
       <main className="main-content">
         <div className="search-section">
