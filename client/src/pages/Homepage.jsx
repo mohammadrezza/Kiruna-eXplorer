01e688bf18093e54231106f177361cf514762d20
SIMONE ANEDDA s330431 <s330431@studenti.polito.it>
2024-11-16 16:30:22 +0100

    homepage buttons (prov)

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import '../style/Homepage.css';
 import { useNavigate } from 'react-router-dom';
 import Slideshow from '../components/Slideshow';
+import { Row ,Col } from 'react-bootstrap';
+import { PiFilePlus, PiFiles, PiMapTrifold} from 'react-icons/pi';
+
 
 function Homepage(props) {
   const navigate = useNavigate();
@@ -28,6 +31,17 @@ function Homepage(props) {
           {props.logged && <button onClick={handleNewDocument}> 
             Create new document
           </button>}
+          <Row>
+            <Col>
+              <button> <PiFiles></PiFiles> List of Documents </button>
+            </Col>
+            <Col>
+              <button> <PiFilePlus></PiFilePlus> Create a document </button>
+            </Col>
+            <Col>
+              <button> <PiMapTrifold></PiMapTrifold> Explore the map </button>
+            </Col>
+          </Row>
         </div>
       </main>
     </div>
