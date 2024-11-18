f859a3908cda501d71ae568060a4feceeda4a139
Alice Santoro s324739 <s324739@studenti.polito.it>
2024-11-18 10:01:51 +0100

    login middleware

--- server/src/routers/sessionRouter.mjs
+++ server/src/routers/sessionRouter.mjs
@@ -1,9 +1,23 @@
 import express from "express";
 import {login, getCurrentSession} from "../Controllers/sessionController.mjs";
+import { body, validationResult } from "express-validator";
 
-const documentRouter = express.Router();
+const sessionRouter = express.Router();
 
-documentRouter.post("/", login);
-documentRouter.get("/", getCurrentSession);
+// Middleware login validation
+const validateLogin = [
+    body('username').isString().notEmpty().withMessage('Username is required'),
+    body('password').isString().notEmpty().withMessage('Password is required'),
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      next();
+    }
+  ];
 
-export default documentRouter;
+sessionRouter.post("/", validateLogin, login);
+sessionRouter.get("/", getCurrentSession);
+
+export default sessionRouter;
