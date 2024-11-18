5cbeabc10fea4234ad0531f9773566c0dd1953fa
mirhajian <mrmirhajian1996@gmail.com>
2024-11-18 15:14:11 +0100

    Update validate request with validator middleware

--- server/src/routers/sessionRouter.mjs
+++ server/src/routers/sessionRouter.mjs
@@ -1,23 +1,15 @@
 import express from "express";
 import {login, getCurrentSession} from "../Controllers/sessionController.mjs";
-import { body, validationResult } from "express-validator";
+import {validator} from "../middlewares/validator.mjs";
+import {body} from "express-validator";
 
 const sessionRouter = express.Router();
 
-// Middleware login validation
-const validateLogin = [
-    body('username').isString().notEmpty().withMessage('Username is required'),
-    body('password').isString().notEmpty().withMessage('Password is required'),
-    (req, res, next) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
-      next();
-    }
-  ];
-
-sessionRouter.post("/", validateLogin, login);
+sessionRouter.post("/",
+    body('username').isString().notEmpty(),
+    body('password').isString().notEmpty(),
+    validator,
+    login);
 sessionRouter.get("/", getCurrentSession);
 
 export default sessionRouter;
