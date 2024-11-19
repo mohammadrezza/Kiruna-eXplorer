5deb3ef0682b738898460202ddeaa4be6f825af2
Alice Santoro s324739 <s324739@studenti.polito.it>
2024-11-19 17:35:13 +0100

    coverage improvement

--- server/tests/unit/services_test/document.test.mjs
+++ server/tests/unit/services_test/document.test.mjs
@@ -4,7 +4,7 @@ import DocumentType from "../../../src/components/documentType.mjs";
 
 const documentService = require('../../../src/services/documentService.mjs');
 import * as documentDao from '../../../src/daos/documentDAO.mjs';
-import {deleteAllConnections} from "../../../src/daos/documentDAO.mjs";
+import { getDocumentWithConnections, getDocumentStakeholders} from "../../../src/daos/documentDAO.mjs";
 
 jest.mock('../../../src/daos/documentDao.mjs');
 afterEach(() => {
@@ -12,7 +12,79 @@ afterEach(() => {
 })
 describe('Services', () => {
 
-    describe('put document', () => {
+    describe('getDocuments', () => {
+        test('should call getAllDocuments with correct parameters and return the result', async () => {
+            const documentId = '123';
+            const title = 'Test Document';
+            const page = 1;
+            const size = 10;
+
+            const mockResult = {
+                data: [
+                    { id: '1', title: 'Document 1' },
+                    { id: '2', title: 'Document 2' }
+                ],
+                pagination: {
+                    total: 2,
+                    totalPages: 1,
+                    currentPage: 1,
+                    size: 10,
+                    hasNextPage: false,
+                    hasPreviousPage: false
+                }
+            };
+
+            documentDao.getAllDocuments.mockResolvedValue(mockResult);
+
+            const result = await documentService.getDocuments(documentId, title, page, size);
+
+            expect(documentDao.getAllDocuments).toHaveBeenCalledWith(documentId, title, page, size);
+            expect(result).toEqual(mockResult);
+        });
+    });
+
+    describe('post document', () => {
+        test('should handle error when creating the document', async () => {
+            const document = {
+                title: 'title',
+                description: 'description',
+                stakeholders: ['stakeholder'],
+                scale: 'scale',
+                issuanceDate: 'issuanceDate',
+                type: DocumentType.DESIGN_DOCUMENT,
+                language: 'language',
+                coordinates: '{"lat": 1.1, "lng": 2.2}',
+                connections: [{id: 'id', type: DocumentConnectionType.PROJECTION}]
+            };
+
+            const errorMessage = 'Error creating the document';
+            jest.spyOn(documentDao, 'addDocument').mockRejectedValue(new Error(errorMessage));
+
+            await expect(documentService.postDocument(
+                document.title,
+                document.description,
+                document.stakeholders,
+                document.scale,
+                document.issuanceDate,
+                document.type,
+                document.language,
+                document.coordinates,
+                document.connections
+            )).rejects.toThrow(`Error creating document: ${errorMessage}`);
+
+            expect(documentDao.addDocument).toHaveBeenCalledWith(
+                expect.any(String),
+                document.title,
+                document.description,
+                document.scale,
+                document.issuanceDate,
+                document.type,
+                document.language,
+                document.coordinates,
+                document.connections.length
+            );
+        });
+
         test('create document with all fields', async () => {
             const document = {
                 title: 'title',
@@ -53,6 +125,7 @@ describe('Services', () => {
     })
 
     describe('put document', () => {
+
         test('update document with all fields', async () => {
             const document = {
                 id: 'id',
@@ -94,5 +167,151 @@ describe('Services', () => {
             expect(result.data.language).toStrictEqual(document.language);
             expect(result.data.coordinates).toStrictEqual(document.coordinates);
         });
+        test('should handle error when updating the document', async () => {
+            const document = {
+                id: 'id',
+                title: 'title',
+                description: 'description',
+                stakeholders: ['stakeholder'],
+                scale: 'scale',
+                issuanceDate: 'issuanceDate',
+                type: DocumentType.DESIGN_DOCUMENT,
+                language: 'language',
+                coordinates: '{"lat": 1.1, "lng": 2.2}',
+                connections: [{id: 'id', type: DocumentConnectionType.PROJECTION}]
+            };
+
+            const errorMessage = 'Error updating the document';
+            jest.spyOn(documentDao, 'editDocument').mockRejectedValue(new Error(errorMessage));
+
+            await expect(documentService.putDocument(
+                document.id,
+                document.title,
+                document.description,
+                document.stakeholders,
+                document.scale,
+                document.issuanceDate,
+                document.type,
+                document.language,
+                document.coordinates,
+                document.connections
+            )).rejects.toThrow(`Error updating document: ${errorMessage}`);
+
+            expect(documentDao.editDocument).toHaveBeenCalledWith(
+                document.id,
+                document.title,
+                document.description,
+                document.scale,
+                document.issuanceDate,
+                document.type,
+                document.language,
+                document.coordinates,
+                document.connections.length
+            );
+        });
+
+
     })
+
+    describe('getDocument', () => {
+        
+        test('should return a document with connections', async () => {
+            const documentId = '123';
+    
+            const mockDocumentData = [
+                {
+                    doc_id: '123',
+                    doc_title: 'Main Document',
+                    doc_description: 'Description of main document',
+                    doc_scale: '1:500',
+                    doc_issuanceDate: '2023-01-01',
+                    doc_type: 'Type1',
+                    doc_language: 'English',
+                    doc_coordinates: '{"lat": 1.1, "lng": 2.2}',
+                    conn_id: '456',
+                    conn_title: 'Connected Document',
+                    conn_description: 'Description of connected document',
+                    conn_scale: '1:1000',
+                    conn_issuanceDate: '2023-02-01',
+                    conn_type: 'Type2',
+                    conn_language: 'French',
+                    conn_coordinates: '{"lat": 3.3, "lng": 4.4}'
+                }
+            ];
+    
+            const mockStakeholders = [
+                { stakeholder: 'Stakeholder1' },
+                { stakeholder: 'Stakeholder2' }
+            ];
+    
+            const mockConnStakeholders = [
+                { stakeholder: 'ConnStakeholder1' },
+                { stakeholder: 'ConnStakeholder2' }
+            ];
+    
+            getDocumentWithConnections.mockResolvedValue(mockDocumentData);
+            getDocumentStakeholders.mockResolvedValueOnce(mockStakeholders).mockResolvedValueOnce(mockConnStakeholders);
+    
+            const result = await documentService.getDocument(documentId);
+    
+            expect(getDocumentWithConnections).toHaveBeenCalledWith(documentId);
+            expect(getDocumentStakeholders).toHaveBeenCalledWith(documentId);
+            expect(getDocumentStakeholders).toHaveBeenCalledWith('456');
+    
+            expect(result).toEqual({
+                id: '123',
+                title: 'Main Document',
+                description: 'Description of main document',
+                stakeholders: ['Stakeholder1', 'Stakeholder2'],
+                scale: '1:500',
+                issuanceDate: '2023-01-01',
+                type: 'Type1',
+                language: 'English',
+                coordinates: { lat: 1.1, lng: 2.2 },
+                connections: [
+                    {
+                        connectionType: 'Type2',
+                        id: '456',
+                        title: 'Connected Document',
+                        description: 'Description of connected document',
+                        stakeholders: ['ConnStakeholder1', 'ConnStakeholder2'],
+                        scale: '1:1000',
+                        issuanceDate: '2023-02-01',
+                        type: 'Type2',
+                        language: 'French',
+                        coordinates: { lat: 3.3, lng: 4.4 }
+                    }
+                ]
+            });
+        });
+
+        test('should handle error when fetching the document', async () => {
+            const documentId = '123';
+            const errorMessage = 'Error fetching document';
+
+            getDocumentWithConnections.mockRejectedValue(new Error(errorMessage));
+
+            await expect(documentService.getDocument(documentId)).rejects.toThrow(`Error fetching document: ${errorMessage}`);
+
+            expect(getDocumentWithConnections).toHaveBeenCalledWith(documentId);
+            expect(getDocumentStakeholders).not.toHaveBeenCalled();
+        });
+
+        test('should return null if documentData is empty', async () => {
+            const documentId = '123';
+
+            getDocumentWithConnections.mockResolvedValue([]);
+            getDocumentStakeholders.mockResolvedValue([]);
+
+            const result = await documentService.getDocument(documentId);
+
+            expect(getDocumentWithConnections).toHaveBeenCalledWith(documentId);
+            expect(getDocumentStakeholders).toHaveBeenCalledWith(documentId);
+            expect(result).toBeNull();
+        });
+
+       
+
+
+    });
 })
